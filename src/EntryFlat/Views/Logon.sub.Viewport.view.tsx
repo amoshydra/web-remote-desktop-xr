879235ce69cda631a2d4c7f6a2a3008a56f837d3
amoshydra <8733840+amoshydra@users.noreply.github.com>
2024-11-30 13:53:43 +0800

    feat: add fallback view
    this can be used when WebRTC stream has failed

--- src/EntryFlat/Views/Logon.sub.Viewport.view.tsx
+++ src/EntryFlat/Views/Logon.sub.Viewport.view.tsx
@@ -1,14 +1,54 @@
+import { useState } from "react";
+import { Button } from "../Components/Button";
 import { CanvasVideoRenderer } from "../Components/CanvasVideoRenderer";
 import { LogonSubViewProps } from "../Components/Logon/interface";
+import { ViewportFallbackRenderer } from "../Components/ViewportFallbackRenderer/ViewportFallbackRenderer";
 
 export const ViewLogonSubViewport = (props: LogonSubViewProps) => {
   const videoElement = props.webControlProps.player?.getMediaElement();
+  const [isFallbackActive, setIsFallbackActive] = useState(false);
+
   return (
-    <div className="bg-neutral-950 w-full h-full">
-      <CanvasVideoRenderer
-        video={videoElement}
-        className="w-full"
-      />
-    </div>
+    <div className="w-full h-full flex flex-col">
+      <div className="shrink p-2 flex justify-end">
+        <Button
+          onClick={async () => {
+            setIsFallbackActive(v => !v);
+          }}
+        >
+          {isFallbackActive ? "Live" : "Fallback"}
+        </Button>
+        <Button
+          onClick={() => {
+            props.webControlProps.resetPlayback();
+          }}
+        >
+          Reset
+        </Button>
+        <Button
+          onClick={() => {
+            props.webControlProps.onMutedChange(v => !v);
+          }}
+        >
+          {props.webControlProps.muted ? "Unmute" : "Mute"}
+        </Button>
+      </div>
+      <div className="neutral-950 w-full h-full grow">
+        {
+          isFallbackActive
+            ? (
+              <ViewportFallbackRenderer
+                className="w-full object-contain object-left-top"
+                obsProps={props.obsProps}
+              />
+            ) : (
+              <CanvasVideoRenderer
+                video={videoElement}
+                className="w-full object-contain object-left-top"
+              />
+            )
+        }
+      </div>
+    </div >
   )
 };
