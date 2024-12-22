ab322b6b67e7b9d3369c763b2b25ae9e2843a93c
amoshydra <8733840+amoshydra@users.noreply.github.com>
2024-12-23 05:55:29 +0800

    chore: setup workflow for deploying the frontend page

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { join, resolve } from "node:path";
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
+const BASE_URL = process.env.BASE_URL || "";
 const workingDir = resolve(import.meta.dirname);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: `${BASE_URL}/`,
+  plugins: [react(), tailwindcss()],
   root: "./packages/client",
   build: {
     outDir: join(workingDir, "dist"),
-  }
-})
+  },
+});
